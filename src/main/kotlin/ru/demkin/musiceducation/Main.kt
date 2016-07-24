fb89eacf0055968908ce3c34319db89c40809bdb
evgen1000end <evgen1000end@gmail.com>
2016-07-24 17:36:00 +0300

    Added gradle project + initial launcher

---
+++ src/main/kotlin/ru/demkin/musiceducation/Main.kt
@@ -0,0 +1,11 @@
+package ru.demkin.musiceducation
+
+/**
+ * Description of ru.demkin.musiceducation
+ * @author evgen1000end
+ * @since 24.07.2016
+ */
+
+fun main(args: Array<String>) {
+    println("Hello, JMusic")
+}
